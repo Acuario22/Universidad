be507c10107784baf7d9708967b4543b2c5a3dcf
Ariel Rios Tapia <149339584+CodeAirt@users.noreply.github.com>
2025-11-03 13:10:15 -0300

    Update clasificacion.py

--- EV2 Data Science/clasificacion.py
+++ EV2 Data Science/clasificacion.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_recall_fscore_support
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #graficos y visualizacion
 import seaborn as sns
 
 #Para que no haya problemas al buscar el dataset
@@ -12,31 +12,32 @@ os.chdir(os.path.dirname(__file__))
 #1. Cargamos nuestro dataset limpio
 df_simce_mat = pd.read_excel("dataset_simce_matematicas_limpio.xlsx")
 
-#2. Definimos X e y (siendo y lo que queremos predecir)
+#2. Definimos X e y (siendo (y) lo que queremos predecir) (x lo que influye en el rendimiento)
 X = df_simce_mat[["cod_grupo", "cod_depe1", "cod_rural_rbd"]]
 y= df_simce_mat["rendimiento_alto"]
 
 #3. Dividimos datos entre entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) # 30% para probarlo y 70% entrenar modelo
 
 #4. Creamos y entrenamos nuestro modelo de regresión logística
-modelo = LogisticRegression(max_iter=1000, random_state=42)
+modelo = LogisticRegression(max_iter=1000, random_state=42) #max iter =asegura que el modelo tenga suficientes iteraciones
 modelo.fit(X_train, y_train)
+# aprende a clasificar si un colegio tiene rendimiento alto o bajo segun las variables
 
 #5. Evaluamos nuestro modelo
-y_pred = modelo.predict(X_test)
+y_pred = modelo.predict(X_test) #el modelo usa los datos de prueba para predecir la clase (alto o bajo rendimiento)
 
 #6. Métricas
-precision = accuracy_score(y_test, y_pred)
-cm = confusion_matrix(y_test, y_pred)
-reporte = classification_report(y_test, y_pred, output_dict=True)
+precision = accuracy_score(y_test, y_pred) #que porcentaje de predicciones fueron correctas
+cm = confusion_matrix(y_test, y_pred) #muestra cuantos casos el modelo clasificó bien y cuantos se equivoco
+reporte = classification_report(y_test, y_pred, output_dict=True) #resume las tres metricas (precision, recall y F1 score)
 
-#6. Preparamos métricas para mostrarlas
+#6. Preparamos métricas para mostrarlas (tabla)
 metrics_df = pd.DataFrame({
     'Clase': ['Bajo (0)', 'Alto (1)'],
-    'Precision': [reporte['0']['precision'], reporte['1']['precision']],
-    'Recall': [reporte['0']['recall'], reporte['1']['recall']],
-    'F1-Score': [reporte['0']['f1-score'], reporte['1']['f1-score']]
+    'Precision': [reporte['0']['precision'], reporte['1']['precision']], #qué tan exactas son las predicciones positivas
+    'Recall': [reporte['0']['recall'], reporte['1']['recall']], #cuántos casos positivos reales detecta
+    'F1-Score': [reporte['0']['f1-score'], reporte['1']['f1-score']] #equilibrio entre precisión y recall
 })
 
 #7. Se muestra el gráfico
@@ -44,18 +45,18 @@ plt.figure(figsize=(14,7))
 #Precisión general
 plt.subplot(1,3,1)
 plt.text(0.5, 0.5, f"Precisión total:\n{precision*100:.2f}%", 
-         fontsize=24, ha='center', va='center', color='green')
+         fontsize=24, ha='center', va='center', color='green') #muestra el porcentaje total de acierto del modelo
 plt.axis('off')
 plt.title("Precisión del modelo de Clasificación")
 #Matriz de confusión
 plt.subplot(1,3,2)
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False) #Visualiza cuántas veces el modelo acertó y cuantas falló
 plt.xlabel("Predicción")
 plt.ylabel("Real")
 plt.title("Matriz de Confusión")
 #Precision, Recall, F1-Score por clase
 plt.subplot(1,3,3)
-metrics_df.set_index('Clase').plot(kind='bar', ax=plt.gca())
+metrics_df.set_index('Clase').plot(kind='bar', ax=plt.gca()) #muestra gráfico de barras comparando las tres métricas para cada clase (rendimiento bajo y alto)
 plt.title("Precision / Recall / F1-score por clase")
 plt.ylabel("Valor")
 plt.ylim(0,1)
